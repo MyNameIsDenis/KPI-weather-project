0096d3d167dce8f1929620e40f2c2123ce9297f6
Denis . <dendj07@gmail.com>
2026-05-04 21:00:31 +0300

    add getData function with API request

--- current_weather/current.js
+++ current_weather/current.js
@@ -2,3 +2,10 @@ const select = document.getElementById('cities');
 let city = select.value;
 
 const input = document.getElementById('enterCity');
+
+function getData() {
+  let xhr = new XMLHttpRequest()
+  let url = `http://api.weatherapi.com/v1/current.json?key=5be5d4c23c00497fb41144549230306&q=${city}&aqi=no`;
+  xhr.open("GET", url);
+  xhr.send();
+}
