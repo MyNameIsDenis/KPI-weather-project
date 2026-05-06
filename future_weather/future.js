627172cc531428081ac21e67dd0bcd8f5bb24949
Denis . <dendj07@gmail.com>
2026-05-06 19:29:42 +0300

    add DOM elements

---
+++ future_weather/future.js
@@ -0,0 +1,5 @@
+const select = document.getElementById('cities');
+let city = select.value;
+let daysSelect = document.getElementById("days");
+let days = daysSelect.value;
+const input = document.getElementById('enterCity');
